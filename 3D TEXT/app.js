dafcaf1b4b92f26db0d9211c23487eddb8728cc2
Anton <82955533+kakakakakakk@users.noreply.github.com>
2021-04-21 17:18:28 +0300

    3D  Text

---
+++ 3D TEXT/app.js
@@ -0,0 +1,29 @@
+let ztxt = new Ztextify(".text", {
+    depth: "30px",
+    layers: 10,
+    fade: false,
+    direction: "both",
+    event: "pointer",
+    eventDirection:"reverse",
+    eventRotation: "45deg"
+});
+
+    let emoji = new Ztextify(".image", {
+    depth: "30px",
+    layers: 10,
+    fade: false,
+    direction: "both",
+    event: "pointer",
+    eventDirection:"default",
+    eventRotation: "45deg"
+});
+
+let img = new Ztextify(".emoji", {
+    depth: "60px",
+    layers: 20,
+    fade: false,
+    direction: "both",
+    event: "pointer",
+    eventDirection:"default",
+    eventRotation: "20deg"
+});
